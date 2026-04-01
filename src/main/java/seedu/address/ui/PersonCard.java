571cfeb57b2591aa38e9b786b34c0e9d7fdbe2fa
junxiancheh <e1354852@u.nus.edu>
2026-04-01 16:36:55 +0800

    Fix checkstyles

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
